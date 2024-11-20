3a072ab2684c01c7c065b02412e32ef55856978a
TigranMelkonyan <tigran.melqonyan96@gmail.com>
2024-11-20 21:14:49 +0400

    Initial commit: putting project into repository.

---
+++ src/main/java/com/tigran/iot_messaging_platform/IotMessagingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tigran.iot_messaging_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotMessagingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotMessagingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tigran/iot_messaging_platform/IotMessagingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tigran.iot_messaging_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotMessagingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
